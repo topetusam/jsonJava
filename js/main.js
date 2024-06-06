9d2cd8e34e05612e62479026b1c21c04f7e871d6
topetusam <67940360+topetusam@users.noreply.github.com>
2024-06-06 08:25:21 -0500

    feat: :tada: Creacion Crud JavaScript

---
+++ js/main.js
@@ -0,0 +1,9 @@
+import { getAllAlbums, addAlbum } from "./module/albums.js";
+import { addPost } from "./module/post.js";
+
+//console.log("await addAlbum({userId:"d0fa", title: "Hola Mundo 2" }));
+
+console.log( await getAllAlbums());
+
+
+    )
---
+++ module/albums.js
@@ -0,0 +1,1 @@
+const validateDeleteAlbum = async

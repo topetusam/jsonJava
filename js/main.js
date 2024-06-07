1121715f52e439430ebb085cae107647a7e3f966
topetusam <67940360+topetusam@users.noreply.github.com>
2024-06-06 21:45:39 -0500

    feat: :rocket: adding POST

--- js/main.js
+++ js/main.js
@@ -1,9 +1,38 @@
-import { getAllAlbums, addAlbum } from "./module/albums.js";
-import { addPost } from "./module/post.js";
+import { getAllAlbums , addAlbum, deleteAlbum, updateAlbum} from "../modules/albums.js";
+import { addPost } from "../module/post.js";
 
-//console.log("await addAlbum({userId:"d0fa", title: "Hola Mundo 2" }));
+let menuAlbums =async() => {
 
-console.log( await getAllAlbums());
+let menu =prompt(`
+¿ Album Menu ?
+    1. Search All
+    2. Add
+    3. Delete
+`, 1);
+menu = Number(menu);
+if(menu == 1) {
+    return await getAllAlbums();
+}
+if(menu ==2) 
+{
+    let userId = prompt("Enter the user id: ", "10")
+    let title = prompt("Enter the album title: ", "Gallery")
+    return await addAlbum({userId, title});
+}
+if(menu ==3){
+    let id= prompt("Enter to ID of the album you want to delete")
+    return await deleteAlbum({id});
+}
 
-
-    )
+}
+let opc = null;
+do{
+    opc = prompt(`
+    ¿ Select an option ?
+        1. Albums
+        2. Posts
+        0. Exit
+    `)
+    opc= Number(opc);
+    if(opc==1) alert(JSON.stringify(await menuAlbums(), null, 4));
+}while(opc)
--- module/albums.js
+++ module/albums.js
@@ -1,1 +1,26 @@
-const validateDeleteAlbum = async
+import { getUser } from "./user.js"
+export const getAllAlbums = async() => {
+    let res = await fetch("http://172.16.101.146:5802/albums");
+    let data = await res.json();
+    return data;
+}
+
+const validateAddAlbum = async({userId, title}) => {
+    if (typeof userId !== "string" || userId === undefined) return { status: 406, mensaje: "userId is invalid"};
+    if (typeof title !== "string" || title === undefined)  return { status: 406, mensaje: "title is invalid" };
+    let user = await getUser ({userId});
+    if(user.status == 204) return {status: 200, message: `El usuario buscado no existe`}
+}
+export const addAlbum = async(arg)=>{
+    let val = await validateAddAlbum(arg);
+    if (val) return val;
+    let config={
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(arg),
+}
+
+let res =await fetch("http://172.16.101.146:5802/albums", config);
+let data = await res.json();
+return data;id
+}
